c1be21d4b797f8f29c4d6f99dd3ed4bf0a3cb3ba
Antonio <antoniogdsm@gmail.com>
2026-05-13 16:51:23 -0300

    aula ate dia 13.05

--- ecommerce1/app/Filament/Resources/Movimentos/Pages/CreateMovimento.php
+++ ecommerce1/app/Filament/Resources/Movimentos/Pages/CreateMovimento.php
@@ -4,8 +4,41 @@ namespace App\Filament\Resources\Movimentos\Pages;
 
 use App\Filament\Resources\Movimentos\MovimentoResource;
 use Filament\Resources\Pages\CreateRecord;
+use App\Models\Produto;
+use Filament\Notifications\Notification;
 
 class CreateMovimento extends CreateRecord
 {
     protected static string $resource = MovimentoResource::class;
+    //hook - verificar se há estoque suficiente
+    protected function beforeCreate(): void
+    {
+        $data = $this->data;
+        $produto = Produto::find($data['produto_id']);
+        $quantidade = (int)$data['quantidade'];
+        $tipo = $data['tipo'];
+
+            if ($tipo === 'saida' && $quantidade > $produto ->estoque){ // -> acessar propriedades e métodos
+                Notification::make()
+                    ->title('Estoque insuficiente')
+                    ->body("Estoque de '{$produto->nome}' é de apenas '{$produto->$estoque}' unidades.")
+                    ->danger() //Estilo vermelho
+                    ->send(); // Enviar a notificação para o usuário no painel
+                $this->halt(); //Execute imediatamente
+            }
+    }
+
+    //criar hook para diminuir o estoque do produto se der certo
+    protected function afterCreate(): void
+    {   
+        $movimento = $this->getRecord();
+        $produto = $movimento->produto;
+        if ($movimento->tipo === 'entrada'){
+            $produto->increment('estoque', $movimento->quantidade);
+
+        } else {
+            $produto->decrement('estoque', $movimento->quantidade);
+        }
+        
+    }
 }
